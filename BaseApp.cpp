750c51c693f35404d8e6328d6c63dc311ff57c0c
Zephnia Mukwa <zmukwa@icloud.com>
2013-06-27 04:05:48 +0100

    this is the latest change in the commit.

---
+++ BaseApp.cpp
@@ -0,0 +1,9 @@
+//
+//  BaseApp.cpp
+//  BaseApp
+//
+//  Created by Zephnia Mukwa on 23/04/2013.
+//  Copyright (c) 2013 Zephnia Mukwa. All rights reserved.
+//
+
+#include "BaseApp.h"
---
+++ BaseApp.h
@@ -0,0 +1,14 @@
+//
+//  BaseApp.h
+//  BaseApp
+//
+//  Created by Zephnia Mukwa on 23/04/2013.
+//  Copyright (c) 2013 Zephnia Mukwa. All rights reserved.
+//
+
+#ifndef __BaseApp__BaseApp__
+#define __BaseApp__BaseApp__
+
+#include <iostream>
+
+#endif /* defined(__BaseApp__BaseApp__) */
