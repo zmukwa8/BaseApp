cafe4e6f1b674c97d5101a5b2679e42c4e7928ab
Zephnia Mukwa <zmukwa@icloud.com>
2013-06-27 04:23:12 +0100

    This is the newest cinder commit

--- BaseApp.cpp
+++ BaseApp.cpp
@@ -7,3 +7,23 @@
 //
 
 #include "BaseApp.h"
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+class BaseApp : public AppBasic {
+public:
+    ￼  void setup();
+    void update();
+    void draw();
+};
+void BaseApp::setup(){}
+void BaseApp::update(){}
+void BaseApp::draw()
+{
+    // clear out the window with black
+    gl::clear( Color( 0, 0, 0 ) );
+}
+
+CINDER_APP_BASIC( BaseApp, RendererGl )
